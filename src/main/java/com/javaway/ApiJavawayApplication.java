736e007710eacc744ad9dcb6364ff919711e8197
kenshivr <Aaladar@proton.me>
2026-04-08 10:05:24 -0600

    feat: initialize Spring Boot project with feature-based structure

---
+++ src/main/java/com/javaway/ApiJavawayApplication.java
@@ -0,0 +1,13 @@
+package com.javaway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJavawayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiJavawayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaway/ApiJavawayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJavawayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
